955d61c836bffcb6de6068d3bc8088ad4405a8f4
qinyj12 <qinyj12@gmail.com>
2020-08-15 18:27:59 +0800

    开荒第三天

--- 1.2.py
+++ 1.2.py
@@ -1,22 +1,27 @@
-# class Students(object):
-#     def __init__(self, list):
-#         self.namelist = list
-#     # 通过for ... in ... 来调用函数
-#     def __iter__(self):
-#         # 如果不返回迭代器的话，似乎会报错
-#         return iter(self.namelist)
+from math import hypot
 
-# class1 = Students(['dog', 'cat'])
-# # 调用自定义的迭代函数
-# for i in class1:
-#     print(i)
-
-# from math import hypot
-
-# class Vector(object):
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
+class Vector(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
     
-#     def __repr__(self):
-#         return 'Vector(%)'
+    # 用print调用
+    def __repr__(self):
+        # %r是把参数原样打印出来，这里用%s其实也是可以的
+        return 'Vector(%r, %r)' % (self.x, self.y)
+    # 用abs()调用
+    def __abs__(self):
+        return hypot(self.x, self.y)
+    # 用 Vector(x, y) + Vector(x, y) 调用，就是普通的加号
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+    # 用for ... in ...调用
+    # 似乎一定要返回一个iter()对象，而根据文档，iter()只能返回iterator即迭代器,而迭代器自身又要有__iter__()方法，晕了。
+    # 反正这里如果直接返回一个list是会报错的，返回迭代器就没问题
+    def __iter__(self):
+        return iter([self.x, self.y])
+
+for i in Vector(3, 4):
+    print(i)
